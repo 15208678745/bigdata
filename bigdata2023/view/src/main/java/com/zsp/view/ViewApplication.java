83aebf7d97547b666838efa7b69150df76d22fc9
Rose <15208678745@163.com>
2023-06-02 00:26:46 +0800

    first commit

---
+++ bigdata2023/view/src/main/java/com/zsp/view/ViewApplication.java
@@ -0,0 +1,14 @@
+package com.zsp.view;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ViewApplication.class, args);
+        System.out.println("hello world");
+    }
+
+}
