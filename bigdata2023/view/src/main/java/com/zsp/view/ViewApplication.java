1458b93b730666cffd345fd42f3bd13536ef0faf
Rose <15208678745@163.com>
2023-06-07 00:10:12 +0800

    forth times

--- bigdata2023/view/src/main/java/com/zsp/view/ViewApplication.java
+++ bigdata2023/view/src/main/java/com/zsp/view/ViewApplication.java
@@ -1,9 +1,14 @@
 package com.zsp.view;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@ComponentScan("com.zsp.view.controller")
+@MapperScan("mybatis")
 public class ViewApplication {
 
     public static void main(String[] args) {
